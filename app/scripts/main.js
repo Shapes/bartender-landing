faf1099affbb81cdae4ad036243e62297f7ddfcd
Marko Balažic <marko.balazic@gmail.com>
2016-12-28 19:55:11 +0100

    modal integration, css to less, basic modal restaurant view

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -74,3 +74,23 @@
 
   // Your custom JavaScript goes here
 })();
+
+
+/* dialog */
+(function() {
+  'use strict';
+  var dialog = document.querySelector('#modal-example');
+  var closeButton = dialog.querySelector('button');
+  var showButton = document.querySelector('#show-modal-example');
+  if (! dialog.showModal) {
+    dialogPolyfill.registerDialog(dialog);
+  }
+  var closeClickHandler = function(event) {
+    dialog.close();
+  };
+  var showClickHandler = function(event) {
+    dialog.showModal();
+  };
+  showButton.addEventListener('click', showClickHandler);
+  closeButton.addEventListener('click', closeClickHandler);
+}());
