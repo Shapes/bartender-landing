ab2ea98efe7a820408335eb483d087952222bcdc
Marko Balažic <marko.balazic@gmail.com>
2016-12-29 20:03:23 +0100

    header, header basic animation, small card style, cover video fix, new cover video

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -94,3 +94,24 @@
   showButton.addEventListener('click', showClickHandler);
   closeButton.addEventListener('click', closeClickHandler);
 }());
+
+
+/* header */
+
+
+function init() {
+  window.addEventListener('scroll', function(e){
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+      shrinkOn = 150,
+      header = document.querySelector("header");
+
+    if (distanceY > shrinkOn) {
+      classie.add(header,"smaller");
+    } else {
+      if (classie.has(header,"smaller")) {
+        classie.remove(header,"smaller");
+      }
+    }
+  });
+}
+window.onload = init();
